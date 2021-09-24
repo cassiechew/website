3e15ae1baeeb7bfa29f700f2bd9cd51ab998a163
Cassie Chew <me@cassie.id>
2021-09-24 23:34:06 +1000

    Adds first animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,25 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
+import { background, ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
 
 function MyApp({ Component, pageProps }: AppProps) : JSX.Element {
 //   const currentPage = React.createContext('/');
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={extendTheme({
+      fonts: {
+        heading: 'monospace',
+        body: 'monospace',
+      },
+      styles: {
+        global: (props: any) => ({
+          body: {
+            bg: mode('white', 'black')(props),
+          },
+        }),
+      },
+    })}
+    >
       <Component {...pageProps} />
     </ChakraProvider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -34,6 +34,18 @@ const cursorFrames = keyframes`
   50% {background-color: #FF0066;}
 `;
 
+const typedFramesMobile = keyframes`
+  from{width: 0;}
+  to{width: 180px;}
+`;
+
+const typedFrames = keyframes`
+  from{width: 0;}
+  to{width: 290px;}
+`;
+
+const typeWidth: string[] = ['180px', '290px'];
+
 const Home: NextPage = () => {
   const [windowHeight, setWindowHeight] = useState(0);
   const [windowWidth, setWindowWidth] = useState(0);
@@ -45,7 +57,10 @@ const Home: NextPage = () => {
     setWindowWidth(visualViewport.width);
   }, []);
 
-  const animation = `${cursorFrames} infinite 1s`;
+  const animation = `${cursorFrames} infinite 0.75s`;
+  const textAnimationMobile = `${typedFramesMobile} 0.75s steps(30,end) 1s 1 normal both`;
+  const textAnimation = `${typedFrames} 0.75s steps(30,end) 1s 1 normal both`;
+  const headingHeight = { base: '54px', lg: '90px' };
 
   return (
     <Flex flexDirection="column" height="screen" width="100%">
@@ -53,65 +68,78 @@ const Home: NextPage = () => {
         <Header />
         <Navbar />
 
-        <Container justifyContent="center">
-          <Flex width="60vw" justifyContent="center" flexDirection="column">
+        <Container ml={{ base: '5', lg: '300' }} justifyContent="center">
+          <Flex width={{ base: '90vw', lg: '60vw' }} justifyContent="center" flexDirection="column">
             <Box alignItems="center">
-              <Box pt={windowHeight / 2.5}>
-                <Text fontFamily="monospace" fontSize="6xl" color="#FF0066">{'<who>'}</Text>
-                <Heading fontFamily="monospace" pl="50px" size="3xl">Hi! I'm Cassie.</Heading>
-                <Text fontFamily="monospace" pl="50px" fontSize="4xl">Full Stack Engineer.</Text>
-                <Flex flexDirection="row">
-                  <Text fontFamily="monospace" fontSize="6xl" color="#FF0066">{'</who> '}</Text>
+              <Box pt={windowHeight * 0.35}>
+                <Flex flexDirection="row" overflow="hidden" height={headingHeight}>
+                  <Text id="typed" animation={{ base: textAnimationMobile, lg: textAnimation }} overflow="hidden" fontSize={{ base: '4xl', lg: '6xl' }} color="#FF0066">{'~/readme '}</Text>
                   <Box
                     animation={animation}
                     ml="5px"
                     borderRadius="5px"
-                    width="60px"
-                    height="auto"
+                    width={{ base: '36px', lg: '60px' }}
+                    height={headingHeight}
                     onClick={() => { router.push('/github'); }}
                   />
-
                 </Flex>
+                {' '}
+                <Heading pl="50px" size="2xl">Hi! I'm Cassie.</Heading>
+                <Text pl="50px" fontSize={{ base: 'md', md: 'lg', lg: '4xl' }}>Full Stack Engineer | Linguist | Gamer</Text>
+
               </Box>
               <Box className="trigger" />
               <Controller>
                 <Box alignItems="center">
                   {/* <Scene triggerElement="trigger" /> */}
                   <Scene
-                    // duration={windowHeight * 2.5}
+                    duration={windowHeight * 1.5}
                     // triggerHook={0.5}
                     pin
                     triggerElement="trigger"
-                    offset={100}
+                    offset={185}
                   >
                     {(progress: number | undefined) => (
                       <Box alignItems="center">
                         <Timeline totalProgress={progress} paused>
                           <Timeline
                             target={(
-                              <Box>
-                                <Text className="topTag" fontSize="6xl" color="#FF0066">{'<cv>'}</Text>
-                                <Heading className="content" pl="50px" size="3xl">Life && work</Heading>
-                                {/* <Text pl="50px" fontSize="4xl">Full Stack Engineer.</Text> */}
-                                <Text className="topTag" fontSize="6xl" color="#FF0066">{'</cv>'}</Text>
-                              </Box>
+                              <>
+                                <Flex flexDirection="row" overflow="hidden" height={headingHeight}>
+                                  <Text fontSize={{ base: '4xl', lg: '6xl' }} color="#FF0066">~/.histrc</Text>
+                                </Flex>
+                                <Heading pl="50px" size="2xl" mr="0">[[ life && work ]] ; then</Heading>
+                              </>
                             )}
                           >
                             <Tween
-                              // target="topTag"
+                              target={0}
                               from={{
-                                yPercent: -50,
-                                // autoAlpha: 0,
-                                rotationY: 90,
-                                // transformOrigin: '50% 50% -100px',
-                                ease: Power3.easeOut,
+                                overflow: 'hidden',
+                                opacity: 0,
+                                width: 0,
                               }}
                               to={{
-                                // autoAlpha: 0,
-                                rotationY: 0,
-                                yPercent: -100,
+                                overflow: 'hidden',
+                                opacity: 1,
+                                width: '100%',
                               }}
-                              duration={0.1}
+                              // duration={0.1}
+                            />
+                            <Tween
+                              target={1}
+                              from={{
+                                overflow: 'hidden',
+                                height: 48,
+                                opacity: 0,
+                                width: 0,
+                              }}
+                              to={{
+                                overflow: 'hidden',
+                                height: 48,
+                                opacity: 1,
+                                width: '200%',
+                              }}
                             />
                           </Timeline>
                         </Timeline>
