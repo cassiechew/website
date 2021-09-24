c42ec196d762ba90d0d9ac3432530f6cf745a628
Cassie Chew <me@cassie.id>
2021-09-24 23:34:06 +1000

    Adds first animation

--- components/To/To.tsx
+++ components/To/To.tsx
@@ -1,6 +1,8 @@
 // Components/ALink/ALink.tsx
 
 import {
+  LinkBox,
+  LinkOverlay,
   Text,
 } from '@chakra-ui/react';
 import Link from 'next/link';
@@ -13,18 +15,20 @@ declare type Props = {
 
 export const To = ({ name, to } : Props) : JSX.Element => {
   const router = useRouter();
-  const color = router.pathname === `/${to}` ? '#FF0066' : 'white';
+  const color = router.pathname === `/${to}` ? '#FF0066' : '#FFFFFF99';
   return (
     <>
-      <Link href={`/${to}`}>
-        <a
-          href={`/${to}`}
-
-        >
-          <Text pr="50px" color={color} fontSize="2xl">{name}</Text>
-
-        </a>
-      </Link>
+      <LinkBox>
+        <LinkOverlay href={`/${to}`}>
+          <Link href={`/${to}`}>
+            <a
+              href={`/${to}`}
+            >
+              <Text pr="50px" color={color} fontSize="2xl">{name}</Text>
+            </a>
+          </Link>
+        </LinkOverlay>
+      </LinkBox>
     </>
   );
 };
--- containers/Navbar/Navbar.tsx
+++ containers/Navbar/Navbar.tsx
@@ -1,6 +1,14 @@
 // Components/Navbar/Navbar.tsx
 
-import { Flex } from '@chakra-ui/react';
+import {
+  Flex, Drawer, Text,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from '@chakra-ui/react';
 import To from '../../components/To';
 
 import Bar from '../../components/Bar';
@@ -10,14 +18,30 @@ declare type Props = {
 }
 
 export const Navbar = () : JSX.Element => (
-  <Flex
-    height="6"
-    alignItems="center"
-    justifyContent="right"
-    pt={9}
-  >
-    <To name="~$ me" to="" />
-    <To name="--work" to="resume" />
-    <To name="--blog" to="blog" />
+  <Flex flexDirection="row" width="100%">
+    <Flex
+      height="6"
+      alignItems="center"
+      justifyContent="right"
+      pt={9}
+      pl="50px"
+    >
+      <Text pr="50px" color="#FF0066" fontSize="2xl">~</Text>
+    </Flex>
+    <Flex
+      height="6"
+      alignItems="center"
+      justifyContent="right"
+      pt={9}
+      width="100%"
+    >
+
+      <To name="~$ me" to="" />
+      <To name="--work" to="resume" />
+      <To name="--blog" to="blog" />
+      <To name="--git" to="github" />
+    </Flex>
+
   </Flex>
+
 );
