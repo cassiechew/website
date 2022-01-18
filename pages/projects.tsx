d13b4740ec51f04815fe93589e2cdfc1c13b215c
Cassie Chew <me@cassie.id>
2022-01-18 15:09:13 +1100

    Refactor of Project code

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -12,6 +12,10 @@ import Header from '../components/Header';
 import { AutoScroll } from '../components/AutoScroll/AutoScroll';
 import { MobileNav } from '../containers/Navbar/Mobile/MobileNavDrawer';
 import { DesktopNav } from '../containers/Navbar/Web/Navbar';
+import { ThisWebsite } from '../containers/Scenes/Projects/ThisWebsite';
+import { Zefr } from '../containers/Scenes/Projects/Zefr';
+import { RssReader } from '../containers/Scenes/Projects/RssReader';
+import { ICalNotion } from '../containers/Scenes/Projects/ICalNotion';
 
 const Projects: NextPage = () => {
   // const [windowHeight, setWindowHeight] = useState(0);
@@ -40,124 +44,10 @@ const Projects: NextPage = () => {
         ml="15vw"
       >
         <Flex flexDirection="column">
-          <Flex
-            flexDirection="column"
-            marginTop="20vh"
-            marginBottom="20px"
-            borderWidth="1px"
-            padding="20px"
-            width="70vw"
-          >
-            <Heading size="xl">This Website</Heading>
-            <Text>This is a portfolio website showcasing some of the frontend skills I have!</Text>
-            <Text>
-              Repo:
-              {' '}
-              <a
-                href="https://github.com/cassiechew/website"
-                target="_blank"
-                rel="noreferrer"
-                style={{
-                  color: '#FF0066',
-                }}
-              >
-                here
-              </a>
-            </Text>
-          </Flex>
-          <Flex
-            flexDirection="column"
-            marginBottom="25px"
-            borderWidth="1px"
-            padding="20px"
-            width="70vw"
-          >
-            <Heading size="xl">Zefr</Heading>
-            <Text>
-              Hosted at
-              {' '}
-              <a
-                href="https://zefr.xyz"
-                target="_blank"
-                rel="noreferrer"
-                style={{
-                  color: '#FF0066',
-                }}
-              >
-                zefr
-              </a>
-              {' '}
-              is a url shortening website writen in NextJs/React with TailwindCss.
-            </Text>
-            <Text>
-              Repo:
-              {' '}
-              <a
-                href="https://github.com/cassiechew/zefr"
-                target="_blank"
-                rel="noreferrer"
-                style={{
-                  color: '#FF0066',
-                }}
-              >
-                here
-              </a>
-            </Text>
-          </Flex>
-          <Flex
-            flexDirection="column"
-            marginBottom="25px"
-            borderWidth="1px"
-            padding="20px"
-            width="70vw"
-          >
-            <Heading size="xl">Rss Reader</Heading>
-            <Text>
-              While it is not currently hosted, it is a project built to act as an RSS feed reader
-              with a relatively simple interface. It is written using; React/NextJs, Graphql,
-              TypeScript, Chakra-UI, and Express + Postgres.
-            </Text>
-            <Text>
-              Repo:
-              {' '}
-              <a
-                href="https://github.com/cassiechew/rss-reader"
-                target="_blank"
-                rel="noreferrer"
-                style={{
-                  color: '#FF0066',
-                }}
-              >
-                here
-              </a>
-            </Text>
-          </Flex>
-          <Flex
-            flexDirection="column"
-            marginBottom="25px"
-            borderWidth="1px"
-            padding="20px"
-            width="70vw"
-          >
-            <Heading size="xl">Notion iCal integration</Heading>
-            <Text>
-              A small, simple overkill server to push iCal data into a notion calendar.
-            </Text>
-            <Text>
-              Repo:
-              {' '}
-              <a
-                href="https://github.com/cassiechew/notion-ical-integration"
-                target="_blank"
-                rel="noreferrer"
-                style={{
-                  color: '#FF0066',
-                }}
-              >
-                here
-              </a>
-            </Text>
-          </Flex>
+          <ThisWebsite />
+          <Zefr />
+          <RssReader />
+          <ICalNotion />
         </Flex>
       </Flex>
       <MobileNav width={width} isOpen={isOpen} onToggle={onToggle} />
